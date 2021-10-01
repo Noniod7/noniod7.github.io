35e6d895d79ba36ce7c5660990caecc8f6ee948d
Noniod7 <41842911+Noniod7@users.noreply.github.com>
2021-10-01 07:34:25 +0700

    Add files via upload

---
+++ zone-h/index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <title>
+        Zone-H Mass
+    </title>
+
+    <!-- Global stylesheets -->
+    <link href="../../external.html?link=https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+    <link href="../assets/css/icons/styles.min.css" rel="stylesheet" type="text/css">
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="../assets/css/bootstrap_limitless.min.css" rel="stylesheet" type="text/css">
+    <link href="../assets/css/layout.min.css" rel="stylesheet" type="text/css">
+    <link href="../assets/css/components.min.css" rel="stylesheet" type="text/css">
+    <link href="../assets/css/colors.min.css" rel="stylesheet" type="text/css">
+
+    <script src="../assets/js/main/jquery.min.js"></script>
+    <script src="../assets/js/main/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/plugins/loaders/blockui.min.js"></script>
+  
+    <script src="../assets/js/plugins/visualization/d3/d3.min.js"></script>
+    <script src="../assets/js/plugins/visualization/d3/d3_tooltip.js"></script>
+    <script src="../assets/js/plugins/forms/styling/switchery.min.js"></script>
+    <script src="../assets/js/plugins/forms/selects/bootstrap_multiselect.js"></script>
+    <script src="../assets/js/plugins/ui/moment/moment.min.js"></script>
+    <script src="../assets/js/plugins/pickers/daterangepicker.js"></script>
+
+    <script src="../assets/js/app.js"></script>
+
+</head>
+<body>
+
+	<div class="navbar navbar-expand-md navbar-dark" style="background-color: black">
+		<div class="navbar-brand">
+			<<img src="../assets/img/niod.png" width="100" height="100"> 
+   </a>
+		</div>
+<b><font face="Courier New">
+		<div class="d-md-none">
+			
+			<button class="navbar-toggler sidebar-mobile-main-toggle" type="button">
+				<i class="icon-paragraph-justify3"></i>
+			</button>
+		</div>
+
+		<div class="collapse navbar-collapse" id="navbar-mobile">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a href="#" class="navbar-nav-link sidebar-control sidebar-main-toggle d-none d-md-block">
+						<i class="icon-paragraph-justify3"></i>
+					</a>
+				</li>
+
+			</ul>
+			<ul class="navbar-nav">
+			<span class="badge bg-success ml-md-3 mr-md-auto">Online
+			</span>
+		    </ul>
+		</div>
+	</div>
+
+	<div class="page-content">
+
+		<div class="sidebar sidebar-dark sidebar-main sidebar-expand-md" style="background-color: black">
+
+		
+			<div class="sidebar-mobile-toggler text-center">
+				<a href="#" class="sidebar-mobile-main-toggle">
+					<i class="icon-arrow-left8"></i>
+				</a>
+				Navigation
+				<a href="#" class="sidebar-mobile-expand">
+					<i class="icon-screen-full"></i>
+					<i class="icon-screen-normal"></i>
+				</a>
+			</div>
+		
+			<div class="sidebar-content">
+
+
+
+		
+				<div class="card card-sidebar-mobile">
+					<ul class="nav nav-sidebar" data-nav-type="accordion">
+
+						
+						<li class="nav-item">
+							<a href="../index.html" class="nav-link active">
+								<i class="icon-home2"></i>
+								<span>
+									HOME
+								</span>
+							</a>
+						</li>
+						<li class="nav-item nav-item-submenu">
+							<a href="#" class="nav-link"><i style="color: red;" class="icon-bug2"></i> <span>DEFACER TOOLS</span></a>
+							<ul class="nav nav-group-sub" data-submenu-title="Layouts">
+								<li class="nav-item">
+	<li class="nav-item">
+	<a href="index.php" class="nav-link">Admin Finder</a>
+</li>
+<li class="nav-item">
+	<a href="../brute-cp" class="nav-link">Bruteforce cPanel</a>
+</li>
+<li class="nav-item">
+	<a href="../brute-fb" class="nav-link">Bruteforce Facebook</a>
+</li>
+<li class="nav-item">
+	<a href="../csrf" class="nav-link">CSRF Online</a>
+</li>
+<li class="nav-item">
+  <a href="../printing-dios" class="nav-link">Printing Dios SQLi</a>
+</li>
+<li class="nav-item">
+	<a href="../webdav" class="nav-link">Webdav Xploiter</a>
+</li>
+</ul>
+</li>
+						<li class="nav-item nav-item-submenu">
+							<a href="#" class="nav-link"><i style="color: #6699FF;" class="icon-embed2"></i> <span>PROGRAMING TOOLS</span></a>
+
+							<ul class="nav nav-group-sub" data-submenu-title="Themes">
+								<li class="nav-item"><a href="../encode-decode/index.html" class="nav-link">Encode - Decode</a></li>
+           <li class="nav-item"><a href="../perapi-html" class="nav-link">Perapi HTML & CSS / C Style</a></li>
+							</ul>
+						</li>
+						<li class="nav-item nav-item-submenu">
+							<a href="#" class="nav-link"><i style="color: yellow;" class="icon-brain"></i> <span>CTF</span></a>
+
+							<ul class="nav nav-group-sub" data-submenu-title="Starter kit">
+								<li class="nav-item"><a href="../../error.html" class="nav-link disabled">Easy to Difficult<span class="badge bg-transparent align-self-center ml-auto">Coming soon</span></a></li>
+							</ul>
+						</li>
+						<li class="nav-item nav-item-submenu">
+							<a href="#" class="nav-link"><i style="color: #666633;" class="icon-mirror"></i> <span>Mirror</span></a>
+							<ul class="nav nav-group-sub" data-submenu-title="Starter kit">
+								<li class="nav-item"><a href="index.php" class="nav-link">Zone-H Notifer</a></li>
+							</ul>
+						</li>
+						<li class="nav-item nav-item-submenu">
+							<a href="#" class="nav-link"><i style="color: #CCFFCC;" class="icon-grid5"></i> <span>Other</span></a>
+
+							<ul class="nav nav-group-sub" data-submenu-title="Themes">
+								<li class="nav-item">
+								<a href="../css-mini" class="nav-link ">CSS Minifier</a>
+							</ul>
+							<ul class="nav nav-group-sub" data-submenu-title="Themes">
+								<li class="nav-item">
+								<a href="../word-coun" class="nav-link ">Word Counter</a></li>
+							</ul>
+						</li>
+
+						<li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">Info</div> <i class="icon-menu-open" title="Forms"></i></li>
+						<li class="nav-item">
+							<a href="../../index2f8a.html" class="nav-link"><i style="color: #FFFFFF;" class="icon-info22"></i> <span>Contact</span></a>
+						</li>
+						<li class="nav-item">
+							<a href="../../indexe476.html" class="nav-link"><i style="color: #FFFFFF;" class="icon-coin-dollar"></i> <span>Donasi</span></a>
+						</li>
+						<li class="nav-item">
+							<a href="../../index90ae.html#beli" class="nav-link"><i style="color: gold;" class="icon-cart5"></i> <span>Buy Tools</span></a>
+						</li>
+										
+
+					</ul>
+				</div>
+
+			</div>
+
+			
+		</div>
+
+		<div class="content-wrapper">
+
+
+			<div class="page-header page-header-light">
+				<div class="page-header-content header-elements-md-inline">
+					
+
+					
+				</div>
+
+				<div class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
+					<div class="d-flex">
+						<div class="breadcrumb">
+							<a href="../index.html" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Home</a>
+							
+						</div>
+					</div>
+
+					
+				</div>
+			</div>        
+        <div class="content">
+            <div class="card">
+                <center><br>
+                <h3 class="tile-title"><i class="fa fa-exclamation-triangle"></i><b>Zone-H Mass</b></h3>
+                
+                
+            </div>
+                    <div class="card">
+                        <div class="card-body">
+                              <center> 
+
+<?php	
+	if($_POST['submit']) {
+		$domain = explode("\r\n", $_POST['url']);
+		$nick =  $_POST['nick'];
+		echo "<font color='white'>Defacer Onhold:</font> <a href='http://www.zone-h.org/archive/notifier=$nick/published=0' target='_blank'>http://www.zone-h.org/archive/notifier=$nick/published=0</a><br>";
+		echo "<font color='white'>Defacer Archive:</font> <a href='http://www.zone-h.org/archive/notifier=$nick' target='_blank'>http://www.zone-h.org/archive/notifier=$nick</a><br><br>";
+		function zoneh($url,$nick) {
+			$ch = curl_init("http://www.zone-h.com/notify/single");
+				  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				  curl_setopt($ch, CURLOPT_POST, true);
+				  curl_setopt($ch, CURLOPT_POSTFIELDS, "defacer=$nick&domain1=$url&hackmode=1&reason=1&submit=Send");
+			return curl_exec($ch);
+				  curl_close($ch);
+		}
+		foreach($domain as $url) {
+			$zoneh = zoneh($url,$nick);
+			if(preg_match("/color=\"red\"> => OK<\/font><\/li>/i", $zoneh)) {
+				echo "<font color='white'>$url -> </font><font color=green> => OK</font><br>";
+			} else {
+				echo "<font color='white'>$url -> </font><font color=red> =× ERROR</font><br>";
+			}
+		}
+	} else {
+		echo "<center>
+		<form method='post'>
+		<input class='masuk' type='text' name='nick' placeholder='Attacker'><br><br>
+		<select>
+    	<option value='Not Available' selected>Not Available</option>
+    	<option value='SQL Injection'>SQL Injection</option>
+    	<option value='URL Poisoning'>URL Poisoning</option>
+    	<option value='File Inclusion'>File Inclusion</option>
+    	<option value='DNS Hijacking'>DNS Hijacking</option>
+    	<option value='DNS Poisoning'>DNS Poisoning</option>
+    	<option value='Admin poor Password'>Admin poor Password</option>
+		</select><br><br>
+		<textarea placeholder='http://site.com/' name='url'></textarea><br><br>
+	<input class='tombol' type='submit' name='submit' value='Submit'>
+		</form>";
+	}
+	echo "</center>";
+	?>
+                                                                     </div>
+        
+     </div>       
+        </div>
+			<center>
+							<div class="navbar  navbar-light">
+				<div class="navbar-collapse" id="">
+					<span class="navbar-text"><font face="Courier New">
+						&copy; 2019. <a href="index.html">Noniod7</a> all rights reserved<br>
+					</span>
+				</div>
+			</div>
+			</center></body>
+</html>
