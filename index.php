20da51551c973939bb9e72738d3c8387a9b87b53
Noniod7 <41842911+Noniod7@users.noreply.github.com>
2021-10-01 07:36:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<script data-ad-client="ca-pub-3066335629028291" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="shortcut icon" type="image/x-icon" href="assets/img/ico.png"/>
+<!-- Global stylesheets -->
+<link href="../external.html?link=https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+<link href="assets/css/icons/styles.min.css" rel="stylesheet" type="text/css">
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="assets/css/bootstrap_limitless.min.css" rel="stylesheet" type="text/css">
+<link href="assets/css/layout.min.css" rel="stylesheet" type="text/css">
+<link href="assets/css/up.css" rel="stylesheet" type="text/css">
+<link href="assets/css/components.min.css" rel="stylesheet" type="text/css">
+<link href="assets/css/colors.min.css" rel="stylesheet" type="text/css">
+<script src="assets/js/main/jquery.min.js"></script>
+<script src="assets/js/main/bootstrap.bundle.min.js"></script>
+<script src="assets/js/plugins/loaders/blockui.min.js"></script>
+<script src="assets/js/plugins/visualization/d3/d3.min.js"></script>
+<script src="assets/js/plugins/visualization/d3/d3_tooltip.js"></script>
+<script src="assets/js/plugins/forms/styling/switchery.min.js"></script>
+<script src="assets/js/plugins/forms/selects/bootstrap_multiselect.js"></script>
+<script src="assets/js/plugins/ui/moment/moment.min.js"></script>
+<script src="assets/js/plugins/pickers/daterangepicker.js"></script>
+<script src="assets/js/app.js"></script>
+<title>Niod-Tech.Com</title>
+</head>
+<body>
+<div class="navbar navbar-expand-md navbar-dark" style="background-color: black">
+	<div class="navbar-brand">
+		<a href="#" class="d-inline-block"><img src="assets/img/niod.png" width="100" height="100"></a>
+	</div>
+	<font face="Courier New">
+	<div class="d-md-none">
+		<button class="navbar-toggler sidebar-mobile-main-toggle" type="button">
+		<i class="icon-paragraph-justify3"></i>
+		</button>
+	</div>
+	<div class="collapse navbar-collapse" id="navbar-mobile">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a href="#" class="navbar-nav-link sidebar-control sidebar-main-toggle d-none d-md-block"><i class="icon-paragraph-justify3"></i></a>
+			</li>
+		</ul>
+		<ul class="navbar-nav">
+			<span class="badge bg-success ml-md-3 mr-md-auto">Online</span>
+		</ul>
+	</div>
+</div>
+<div class="page-content">
+	<div class="sidebar sidebar-dark sidebar-main sidebar-expand-md" style="background-color: black">
+		<div class="sidebar-mobile-toggler text-center">
+			<a href="#" class="sidebar-mobile-main-toggle"><i class="icon-arrow-left8"></i></a>
+			Tools.Niod-Tech.Com <a href="#" class="sidebar-mobile-expand">
+			<i class="icon-screen-full"></i>
+			<i class="icon-screen-normal"></i>
+			</a>
+		</div>
+		<div class="sidebar-content">
+			<div class="card card-sidebar-mobile">
+				<ul class="nav nav-sidebar" data-nav-type="accordion">
+					<li class="nav-item">
+						<a href="index.php" class="nav-link active">
+						<i class="icon-home2"></i>
+						<span>HOME</span>
+						</a>
+					</li>
+					<li class="nav-item nav-item-submenu">
+						<a href="#" class="nav-link"><i style="color: red;" class="icon-bug2"></i><span>DEFACER TOOLS</span></a>
+						<ul class="nav nav-group-sub" data-submenu-title="Layouts">
+						 <li class="nav-item">
+	<a href="/admin-find" class="nav-link">Admin Finder</a>
+</li>
+<li class="nav-item">
+	<a href="/brute-cp" class="nav-link">Bruteforce cPanel</a>
+</li>
+<li class="nav-item">
+	<a href="/brute-fb" class="nav-link">Bruteforce Facebook</a>
+</li>
+<li class="nav-item">
+	<a href="../csrf" class="nav-link">CSRF Online</a>
+</li>
+<li class="nav-item">
+  <a href="/printing-dios" class="nav-link">Printing Dios SQLi</a>
+</li>
+<li class="nav-item">
+	<a href="/webdav" class="nav-link">Webdav Xploiter</a>
+</li>
+</ul>
+</li>
+					<li class="nav-item nav-item-submenu">
+						<a href="#" class="nav-link"><i style="color: #6699FF;" class="icon-embed2"></i><span>PROGRAMING TOOLS</span></a>
+						<ul class="nav nav-group-sub" data-submenu-title="Themes">
+							<li class="nav-item">
+								<a href="shshshs" class="nav-link">Encode - Decode</a>
+							</li>
+							<li class="nav-item">
+								<a href="perapi-html" class="nav-link">Perapi HTML & CSS / C Style</a>
+							</li>
+						</ul>
+					</li>
+					<li class="nav-item nav-item-submenu">
+						<a href="#" class="nav-link"><i style="color: yellow;" class="icon-brain"></i><span>CTF</span></a>
+						<ul class="nav nav-group-sub" data-submenu-title="Starter kit">
+							<li class="nav-item">
+								<a href="error.html" class="nav-link disabled">Easy to Difficult<span class="badge bg-transparent align-self-center ml-auto">Coming soon</span></a>
+							</li>
+						</ul>
+					</li>
+					<li class="nav-item nav-item-submenu">
+						<a href="#" class="nav-link"><i style="color: #666633;" class="icon-mirror"></i><span>Mirror</span></a>
+						<ul class="nav nav-group-sub" data-submenu-title="Starter kit">
+							<li class="nav-item">
+								<a href="/zone-h" class="nav-link">Zone-H Notifer</a>
+							</li>
+						</ul>
+					</li>
+					
+					<li class="nav-item nav-item-submenu">
+						<a href="#" class="nav-link"><i style="color: #CCFFCC;" class="icon-grid5"></i><span>Other</span></a>
+						<ul class="nav nav-group-sub" data-submenu-title="Themes">
+							<li class="nav-item">
+								<a href="../css-mini" class="nav-link ">CSS Minifier</a>
+							</ul>
+							<ul class="nav nav-group-sub" data-submenu-title="Themes">
+								<li class="nav-item">
+								<a href="../word-coun" class="nav-link ">Word Counter</a></li>
+							</ul>
+						</li>
+					<li class="nav-item-header">
+						<div class="text-uppercase font-size-xs line-height-xs">Info</div>
+						<i class="icon-menu-open" title="Forms"></i>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link"><i style="color: #FFFFFF;" class="icon-info22"></i><span>Contact</span></a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link"><i style="color: #FFFFFF;" class="icon-coin-dollar"></i><span>Donasi</span></a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link"><i style="color: gold;" class="icon-cart5"></i><span>Buy Tools</span></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="content-wrapper">
+		<div class="page-header page-header-light">
+			<div class="page-header-content header-elements-md-inline"></div>
+			<div class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
+				<div class="d-flex">
+					<div class="breadcrumb">
+						<a href="index.php" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Home</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="content">
+			<div class="card">
+				<div class="card-body">
+					<h3 class="tile-title">
+					<i class="fa fa-exclamation-triangle"></i><center><img src="img/hackedbynoniod7.png" width="170" height="170"></div>
+			</div>
+			
+			<div class="content">
+			<div class="card">
+				<div class="card-body">
+					<h3 class="tile-title">
+					<i class="fa fa-exclamation-triangle"></i>
+					<center><img src="img/bangka.png" width="75" height="75">
+					    <img src="img/cct.png" width="85" height="85">
+					       <img src="img/cct.png" width="65" height="65">
+					</div>
+			</div>
+			
+			<div class="card">
+				<div class="card-header header-elements-sm-inline">
+					<center>
+					<a href="https://wa.me/6282231428804?text=noniod7"><img src="img/wa.png" width="50" height="50"></a>
+					<a href="https://fb.me/noniod7"><img src="img/fb.png" width="50" height="50"></a>
+					<a href="https://ig.me/noniod7"><img src="img/ig.png" width="50" height="50"></a></center>
+				</div>
+			</div>
+		</div>
+		<center>
+		<div class="navbar navbar-light">
+			<div class="navbar-collapse" id="">
+				<span class="navbar-text"><font face="Courier New">
+				&copy; 2019. <a href="index.php">Noniod7</a> all rights reserved<br>
+</span>
+			</div>
+		</div>
+		</center>
+		</body>
+		</html>
